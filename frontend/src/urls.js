0089fc08e1fb638207d99e34beb484fe0db77fa7
yashas123 <rameshmamathayashas@gmail.com>
2020-03-05 20:09:59 +0530

    remove detect_text function
    Former-commit-id: faa5daec781bef60766c345e9763cf3165de2c3b

--- frontend/src/urls.js
+++ frontend/src/urls.js
@@ -1,10 +1,3 @@
-import Location from "react-app-location";
-
-const urls = {
-	HomeLocation: "/",
-	ResultLocation : "/result"
-};
-
 const djangoServerUrl = "http://127.0.0.1:8000/";
 
 const djangoUrls = {
@@ -13,8 +6,4 @@ const djangoUrls = {
 
 for (let url in djangoUrls) djangoUrls[url] = djangoServerUrl + djangoUrls[url];
 
-for (let url in urls) urls[url] = new Location(urls[url]);
-
-export { djangoUrls };
-
-export default urls;
+export default djangoUrls;
