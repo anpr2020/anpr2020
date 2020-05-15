1e0782ff73df7f378ac05baf078c718c929c3074
yashas123 <rameshmamathayashas@gmail.com>
2020-05-15 19:07:37 +0530

    update-8 Livejs
    Former-commit-id: b7ebd74de8101fb02b1fc325e7f0d2452b4650cf

--- core/tasks.py
+++ core/tasks.py
@@ -18,6 +18,85 @@ import matplotlib.pyplot as plt
 from imutils.object_detection import non_max_suppression
 #project import end
 
+def frame_process(success, frame):
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    canny_output = cv2.Canny(gray, 170, 200)
+
+
+    contours, hierarchy = cv2.findContours(
+        canny_output, cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)
+
+    contours = sorted(contours, key=cv2.contourArea, reverse=True)
+
+    plate_match = False
+    for contour in contours:
+        approx = cv2.approxPolyDP(
+            contour, cv2.arcLength(contour, True) * 0.05, True)
+
+        if len(approx) >= 4 and np.abs(cv2.contourArea(contour)) > 2000:
+            rect = cv2.minAreaRect(contour)
+            box = np.int0(cv2.boxPoints(rect))
+            (box_w, box_h) = helper_boxwh(box)
+
+            accepted_ratio = 4.16
+            error = 0.84
+
+            ratio = box_w / box_h
+
+            if accepted_ratio - error < ratio and ratio < accepted_ratio + error:
+                cv2.drawContours(
+                    frame, [box, contour, approx], 2, (0, 0, 255), 5)
+                x, y, w, h = cv2.boundingRect(approx)
+                plate_frame = frame[y:y+h,x:x+w]
+                rec_text = get_text_from_frame(plate_frame)
+
+                if not rec_text:
+                    continue
+
+                plate_match = True
+                cv2.putText(frame, rec_text, (x, y),
+                            cv2.FONT_HERSHEY_SIMPLEX , 1.5, (0, 255, 0), 2, cv2.LINE_AA)
+
+    drawing = np.zeros(
+        (canny_output.shape[0], canny_output.shape[1], 3), dtype=np.uint8)
+
+    for contour in contours:
+        color = [random.randint(0, 255), random.randint(0, 255), random.randint(0, 255)]
+        cv2.drawContours(drawing, contour, -1, color)
+
+    plate = frame.copy()
+
+    return canny_output, drawing, plate, rec_text, plate_match
+
+@task(bind=True)
+def recognize_live(self, video_path):
+    total = count = 0
+    info_data = {
+        'frame': [],
+        'canny': [],
+        'contour': [],
+        'plate': [],
+        'text': [],
+    }
+
+    success, frame = video_cap.read()
+    while success:
+        total += 1
+        canny_output, drawing, plate, rec_text, plate_match = frame_process(success, frame)
+        info_append('frame', frame)
+        info_append('canny', canny_output)
+        info_append('contour', drawing)
+        info_append('plate', plate)
+        info_append('text', rec_text, 0)
+
+        if plate_match:
+            count += 1
+
+        self.update_state(state="RUNNING", meta=info_data)
+        success, frame = video_cap.read()
+
+    return info_data
+
 @task(bind=True)
 def recognize(self, video_path):
 
@@ -74,63 +153,21 @@ def recognize(self, video_path):
             v = 'data:image/jpeg;base64,' + base64.b64encode(cv2.imencode('.jpg', v)[1]).decode("utf-8")
         info_data[k].append(v)
 
+    success, frame = video_cap.read()
+
     while success:
         total += 1
+        canny_output, drawing, plate, rec_text, plate_match = frame_process(success, frame)
         info_append('frame', frame)
-        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-        canny_output = cv2.Canny(gray, 170, 200)
         info_append('canny', canny_output)
-
-        contours, hierarchy = cv2.findContours(
-            canny_output, cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)
-
-        contours = sorted(contours, key=cv2.contourArea, reverse=True)
-
-        plate_match = False
-        for contour in contours:
-            approx = cv2.approxPolyDP(
-                contour, cv2.arcLength(contour, True) * 0.05, True)
-
-            if len(approx) >= 4 and np.abs(cv2.contourArea(contour)) > 2000:
-                rect = cv2.minAreaRect(contour)
-                box = np.int0(cv2.boxPoints(rect))
-                (box_w, box_h) = helper_boxwh(box)
-
-                accepted_ratio = 4.16
-                error = 0.84
-
-                ratio = box_w / box_h
-
-                if accepted_ratio - error < ratio and ratio < accepted_ratio + error:
-                    cv2.drawContours(
-                        frame, [box, contour, approx], 2, (0, 0, 255), 5)
-                    x, y, w, h = cv2.boundingRect(approx)
-                    plate_frame = frame[y:y+h,x:x+w]
-                    rec_text = get_text_from_frame(plate_frame)
-
-                    if not rec_text:
-                        continue
-
-                    plate_match = True
-                    cv2.putText(frame, rec_text, (x, y),
-                                cv2.FONT_HERSHEY_SIMPLEX , 1.5, (0, 255, 0), 2, cv2.LINE_AA)
-                    info_append('text', rec_text, 0)
+        info_append('contour', drawing)
+        info_append('plate', plate)
+        info_append('text', rec_text, 0)
 
         if plate_match:
             count += 1
 
-        drawing = np.zeros(
-            (canny_output.shape[0], canny_output.shape[1], 3), dtype=np.uint8)
-
-        for contour in contours:
-            color = [random.randint(0, 255), random.randint(0, 255), random.randint(0, 255)]
-            cv2.drawContours(drawing, contour, -1, color)
-
-        info_append('contour', drawing)
-        info_append('plate', frame)
-
         self.update_state(state="RUNNING", meta=info_data)
-
         success, frame = video_cap.read()
 
     video_cap.release()
--- frontend/src/pages/Live.js
+++ frontend/src/pages/Live.js
@@ -1,31 +1,313 @@
 import React from "react";
 
-import { Container } from "@material-ui/core";
+import { withStyles } from "@material-ui/core/styles";
+import {
+  Button,
+  Container,
+  FormControl,
+  InputLabel,
+  TextField,
+  Typography,
+  Switch,
+  MenuItem,
+  Select,
+  Grid,
+} from "@material-ui/core";
+import VideocamIcon from "@material-ui/icons/Videocam";
 
-export default class Live extends React.Component {
+const styles = {
+  formControl: {
+    minWidth: 120,
+  },
+};
+
+class Live extends React.Component {
   constructor(props) {
     super(props);
+    this.select = React.createRef();
     this.videoSource = React.createRef();
+    this.state = {
+      sourceSwitch: false,
+      ipSwitch: false,
+      sourceType: false,
+      selectOptions: [],
+      selectValue: "",
+      streamSource: "",
+      sourceValue: "",
+      sourceUrl: "",
+      sourceIP: "",
+      sourcePort: 0,
+    };
   }
 
-  componentDidMount() {
-    if (navigator.mediaDevices.getUserMedia) {
+  stopMediaTracks = (stream) => {
+    const streamTracks = stream.getTracks();
+    streamTracks.forEach((track) => {
+      track.stop();
+    });
+  };
+
+  gotDevices = (mediaDevices) => {
+    let selectOptions = this.state.selectOptions || [];
+    if (selectOptions.length === 0)
+      mediaDevices.forEach((mediaDevice, i) => {
+        if (mediaDevice.kind === "videoinput") {
+          selectOptions.push(
+            <MenuItem key={i} value={mediaDevice.deviceId || i}>
+              {mediaDevice.label || `Camera ${i}`}
+            </MenuItem>
+          );
+        }
+      });
+    this.setState({
+      selectOptions: selectOptions || "",
+    });
+  };
+
+  onChangeLiveSource = () => {
+    const {
+      sourceSwitch,
+      streamSource,
+      selectValue,
+      sourceValue,
+    } = this.state;
+
+    if (sourceSwitch) {
+      this.videoSource.current.src = sourceValue;
+    } else {
+      if (!selectValue) return;
+      if (streamSource) this.stopMediaTracks(streamSource);
+
+      const constraints = {
+        video: {
+          deviceId: { exact: selectValue },
+        },
+        audio: false,
+      };
+
       navigator.mediaDevices
-        .getUserMedia({ video: true })
+        .getUserMedia(constraints)
         .then((stream) => {
+          this.setState({
+            streamSource: stream,
+          });
           this.videoSource.current.srcObject = stream;
         })
-        .catch((err) => {
-          alert("ERROR " + err);
+        .catch((error) => {
+          console.error(error);
         });
+    }
+  };
+
+  componentDidMount() {
+    try {
+      navigator.getUserMedia =
+        navigator.getUserMedia ||
+        navigator.webkitGetUserMedia ||
+        navigator.mozGetUserMedia ||
+        navigator.msGetUserMedia ||
+        navigator.oGetUserMedia;
+      if (!navigator.getUserMedia) return alert("No streaming devices found");
+      navigator.mediaDevices.enumerateDevices().then(this.gotDevices);
+    } catch (err) {
+      alert("ERROR : " + err.message);
+      console.log(err);
     }
   }
 
+  handleSelectChange = (e) => {
+    const selectValue = e.target.value;
+    this.setState(
+      {
+        selectValue: selectValue,
+      },
+      () => this.onChangeLiveSource()
+    );
+  };
+
+  handleSwitchChange = (e) =>
+    this.setState({ [e.target.name]: e.target.checked });
+
+  handleSourceInput = (e) => {
+    const { sourceIP, sourcePort } = this.state;
+    const { name, value } = e.target;
+
+    this.setState(
+      {
+        [name]: value,
+      },
+      () => {
+        this.setState(
+          {
+            sourceValue: ["sourceIP", "sourcePort"].includes(name)
+              ? `http://${sourceIP}:${sourcePort}`
+              : value,
+          },
+          () => this.onChangeLiveSource()
+        );
+      }
+    );
+  };
+
   render() {
+    const { classes } = this.props;
+    const {
+      sourceSwitch,
+      sourceType,
+      ipSwitch,
+      selectOptions,
+      selectValue,
+      sourceValue,
+      sourceUrl,
+      sourceIP,
+      sourcePort,
+    } = this.state;
+
     return (
       <Container maxWidth="xl">
-        <video width="100%" ref={this.videoSource} autoPlay />
+        <Grid
+          container
+          spacing={3}
+          justify="center"
+          alignItems="center"
+          style={{ textAlign: "center" }}
+        >
+          <Grid item md={3}>
+            <FormControl variant="outlined" className={classes.formControl}>
+              <InputLabel id="source-select-label">Source</InputLabel>
+              <Select
+                labelId="source-select-label"
+                id="source-select"
+                value={selectValue || ""}
+                onChange={this.handleSelectChange}
+                label="Source"
+                ref={this.select}
+                disabled={sourceSwitch}
+              >
+                <MenuItem value="">
+                  <em>None</em>
+                </MenuItem>
+                {selectOptions}
+              </Select>
+            </FormControl>
+          </Grid>
+          <Grid item md={2}>
+            <Typography component="div">
+              <Grid container justify="center" alignItems="center">
+                <Grid item>Devices</Grid>
+                <Grid item>
+                  <Switch
+                    name="sourceSwitch"
+                    checked={sourceSwitch}
+                    onChange={this.handleSwitchChange}
+                  />
+                </Grid>
+                <Grid item>URL/IP:Port</Grid>
+              </Grid>
+            </Typography>
+          </Grid>
+          <Grid item md={7}>
+            <Grid container justify="center" alignItems="center">
+              <Grid item xs={12}>
+                {ipSwitch ? (
+                  <TextField
+                    name="sourceUrl"
+                    label="Source URL"
+                    disabled={!sourceSwitch}
+                    value={sourceUrl}
+                    onChange={this.handleSourceInput}
+                  />
+                ) : (
+                  <Grid container justify="center" alignItems="center">
+                    <Grid item xs={8}>
+                      <TextField
+                        name="sourceIP"
+                        label="IP"
+                        disabled={!sourceSwitch}
+                        value={sourceIP}
+                        onChange={this.handleSourceInput}
+                      />
+                    </Grid>
+                    <Grid item xs={4}>
+                      <TextField
+                        type="number"
+                        name="sourcePort"
+                        label="Port"
+                        disabled={!sourceSwitch}
+                        value={sourcePort}
+                        onChange={this.handleSourceInput}
+                      />
+                    </Grid>
+                  </Grid>
+                )}
+              </Grid>
+              <Grid item xs={6}>
+                <Typography component="div">
+                  <Grid container justify="center" alignItems="center">
+                    <Grid item>IP:Port</Grid>
+                    <Grid item>
+                      <Switch
+                        name="ipSwitch"
+                        checked={ipSwitch}
+                        onChange={this.handleSwitchChange}
+                        disabled={!sourceSwitch}
+                      />
+                    </Grid>
+                    <Grid item>URL</Grid>
+                  </Grid>
+                </Typography>
+              </Grid>
+              <Grid item xs={6}>
+                <Typography component="div">
+                  <Grid container justify="center" alignItems="center">
+                    <Grid item>Video</Grid>
+                    <Grid item>
+                      <Switch
+                        name="sourceType"
+                        checked={sourceType}
+                        onChange={this.handleSwitchChange}
+                        disabled={!sourceSwitch}
+                      />
+                    </Grid>
+                    <Grid item>Image</Grid>
+                  </Grid>
+                </Typography>
+              </Grid>
+            </Grid>
+          </Grid>
+          <Grid item xs={12}>
+            {sourceType ? (
+              <img
+                width="100%"
+                key={0}
+                alt=""
+                ref={this.videoSource}
+                src={sourceValue}
+              />
+            ) : (
+              <video
+                width="100%"
+                key={0}
+                ref={this.videoSource}
+                autoPlay
+                controls
+              ></video>
+            )}
+          </Grid>
+          <Grid item xs={12}>
+            <Button
+              size="large"
+              variant="contained"
+              color="primary"
+              startIcon={<VideocamIcon />}
+            >
+              Start Streaming
+            </Button>
+          </Grid>
+        </Grid>
       </Container>
     );
   }
 }
+
+export default withStyles(styles)(Live);
